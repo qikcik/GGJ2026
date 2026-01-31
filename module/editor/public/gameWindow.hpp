7c4e56cb85f7d42ea1e38216adf81a71b606c984
qikcik <qikcik@gmail.com>
2026-01-31 16:46:07 +0100

    wierd hack to imporve

--- module/editor/public/gameWindow.hpp
+++ module/editor/public/gameWindow.hpp
@@ -221,10 +221,11 @@ inline void GameWindow::onUpdate(float deltaTime) {
         rlSetUniform(shadowMapLoc, &textureActiveSlot, SHADER_UNIFORM_INT, 1);
 
         BeginMode3D(camera);
+            DrawGrid(100,1);
             draw();
-            //if (auto p = editorCtx.selectedModel.lock()) {
-            //    DrawBoundingBox(p->boundingBox(), RED );
-            //}
+            if (auto p = editorCtx.selectedModel.lock()) {
+                DrawBoundingBox(p->boundingBox(), RED );
+            }
         EndMode3D();
         DrawFPS(10,10);
     });
